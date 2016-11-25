4d26673b1159514043fe8594ba1f64757ace0fdc
Yannick Scherer <yannick.scherer@stylefruits.de>
2016-11-25 15:27:41 +0100

    Version 0.1.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject alumbra/analyzer "0.1.2"
+(defproject alumbra/analyzer "0.1.3-SNAPSHOT"
   :description "GraphQL Schema/Operation Transformations"
   :url "https://github.com/alumbra/alumbra.analyzer"
   :license {:name "MIT License"
