083500b052fb30edab8c12e1925c9115ccc77def
Yannick Scherer <yannick.scherer@stylefruits.de>
2017-04-26 14:49:24 +0200

    upgrade dependencies.

--- project.clj
+++ project.clj
@@ -8,14 +8,14 @@
   :dependencies [[org.clojure/clojure "1.9.0-alpha14" :scope "provided"]
                  [alumbra/spec "0.1.6" :scope "provided"]
                  [com.stuartsierra/dependency "0.2.0"]
-                 [com.rpl/specter "1.0.0"]]
+                 [com.rpl/specter "1.0.1"]]
   :profiles {:dev
              {:dependencies [[org.clojure/test.check "0.9.0"]
                              [alumbra/parser "0.1.6"]
                              [alumbra/generators "0.2.2"]]}
              :codox
              {:plugins [[lein-codox "0.10.0"]]
-              :dependencies [[codox-theme-rdash "0.1.1"]]
+              :dependencies [[codox-theme-rdash "0.1.2"]]
               :codox {:project {:name "alumbra.analyzer"}
                       :metadata {:doc/format :markdown}
                       :themes [:rdash]
