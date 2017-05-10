66af8a3b49922f935a7786bd1c79503392bf49df
Yannick Scherer <yannick.scherer@stylefruits.de>
2017-05-10 17:19:50 +0200

    include ':default-value' key for analyzed schema arguments.

--- src/alumbra/analyzer/types/arguments.clj
+++ src/alumbra/analyzer/types/arguments.clj
@@ -2,15 +2,22 @@
   (:require [alumbra.analyzer.types.type-description
              :refer [describe-type]]
             [alumbra.analyzer.inline-directives
-             :refer [read-inline-directives]]))
+             :refer [read-inline-directives]]
+            [alumbra.analyzer.value
+             :refer [read-value]]))
 
 (defn read-arguments
   [arguments]
   (reduce
     (fn [result {:keys [alumbra/argument-name
-                        alumbra/argument-type] :as x}]
+                        alumbra/argument-type
+                        alumbra/default-value]
+                 :as argument}]
       (->> {:argument-name     argument-name
-            :inline-directives (read-inline-directives x)}
-           (merge (describe-type argument-type))
+            :inline-directives (read-inline-directives argument)}
+           (merge
+             (describe-type argument-type)
+             (when default-value
+               {:default-value (read-value default-value)}))
            (assoc result argument-name)))
     {} arguments))
