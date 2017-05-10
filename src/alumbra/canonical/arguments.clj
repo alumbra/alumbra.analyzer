dd06af9e67c277df87c6c9ec228dffa8bafebb92
Yannick Scherer <yannick.scherer@stylefruits.de>
2017-05-10 17:27:46 +0200

    fall back to default value if argument is missing.

--- src/alumbra/canonical/arguments.clj
+++ src/alumbra/canonical/arguments.clj
@@ -5,22 +5,22 @@
 
 (defn- add-default-values
   [opts argument-types result]
-  (reduce
-    (fn [result [argument-name {:keys [default-value] :as type}]]
-      (if (and default-value
-               (not (contains? result argument-name)))
-        (assoc result
-               argument-name
-               (resolve-literal-value opts type default-value))
-        result))
-    result argument-types))
+  (let [missing-arguments (apply dissoc argument-types (keys result))]
+    (reduce
+      (fn [result [argument-name {:keys [default-value] :as type}]]
+        (if (and default-value
+                 (not (contains? result argument-name)))
+          (assoc result
+                 argument-name
+                 (resolve-literal-value opts type default-value))
+          result))
+      result missing-arguments)))
 
 (defn resolve-arguments
   [opts argument-types arguments]
   (->> (for [{:keys [alumbra/argument-name
                      alumbra/argument-value]} arguments
-             :let [{:keys [type-description]}
-                   (get argument-types argument-name)]]
+             :let [{:keys [type-description]} (get argument-types argument-name)]]
          [argument-name (resolve-value opts type-description argument-value)])
        (into {})
        (add-default-values opts argument-types)))
